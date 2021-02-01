dcfcb7429d3579c3102b79e4e5c54cdea4bf64d2
Nikita Ustinov <65974693+NikitaUstnov@users.noreply.github.com>
2021-02-02 00:43:14 +0200

    fix

---
+++ build/precache-manifest.0a74761114ba4abee0d9d9f4c60b9624.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc0efaac08057101e532abad6eb25884",
+    "url": "/goit-react-hw-02-phonebook-rebild/index.html"
+  },
+  {
+    "revision": "dd6e0e45d8ee065261c7",
+    "url": "/goit-react-hw-02-phonebook-rebild/static/css/main.67f78597.chunk.css"
+  },
+  {
+    "revision": "f4151cca25f84c0fd69f",
+    "url": "/goit-react-hw-02-phonebook-rebild/static/js/2.14b0ad3f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-02-phonebook-rebild/static/js/2.14b0ad3f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd6e0e45d8ee065261c7",
+    "url": "/goit-react-hw-02-phonebook-rebild/static/js/main.34ed27de.chunk.js"
+  },
+  {
+    "revision": "b8f4d1b5995cc901d223",
+    "url": "/goit-react-hw-02-phonebook-rebild/static/js/runtime-main.dceee536.js"
+  }
+]);
