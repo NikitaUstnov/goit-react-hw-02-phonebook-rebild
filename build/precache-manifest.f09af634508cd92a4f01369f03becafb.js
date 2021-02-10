3aae4e550bc63332059b55e553dedf6ebb9d232e
Nikita Ustinov <65974693+NikitaUstnov@users.noreply.github.com>
2021-02-10 02:08:42 +0200

    for 3 homework

--- build/precache-manifest.f09af634508cd92a4f01369f03becafb.js
+++ build/precache-manifest.f09af634508cd92a4f01369f03becafb.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "fc0efaac08057101e532abad6eb25884",
+    "revision": "49149e93976badd9c8efee87321eceb5",
     "url": "/goit-react-hw-02-phonebook-rebild/index.html"
   },
   {
-    "revision": "dd6e0e45d8ee065261c7",
+    "revision": "94297c09c25260af93e9",
     "url": "/goit-react-hw-02-phonebook-rebild/static/css/main.67f78597.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/goit-react-hw-02-phonebook-rebild/static/js/2.14b0ad3f.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "dd6e0e45d8ee065261c7",
-    "url": "/goit-react-hw-02-phonebook-rebild/static/js/main.34ed27de.chunk.js"
+    "revision": "94297c09c25260af93e9",
+    "url": "/goit-react-hw-02-phonebook-rebild/static/js/main.c996ee2c.chunk.js"
   },
   {
     "revision": "b8f4d1b5995cc901d223",
